bf0a30010a562090f419e60c3f7275408844637d
NETRANSHI TRIPATHI <netranshitripathi@gmail.com>
2024-10-15 22:00:38 +0530

    Time: 84 ms (93.02%), Space: 13 MB (69.77%) - LeetHub

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def minimumSteps(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ans = 0
+        blackCount = 0 # Tracks the number of black balls (1s)
+
+        # Traverse through the string
+        for ch in s:
+            if ch == '0':
+                # White ball encountered, add the number of black balls on its left
+                ans += blackCount
+            else:
+                # Black ball encountered, increment the black ball count
+                blackCount += 1
+
+        return ans
+        
